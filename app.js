cde3639645b4d06973fd5a4b06bea1968aa7f6ea
James Sample <jamesgsample@gmail.com>
2023-03-02 07:38:21 +0000

    found how to get specific tram stop data

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ const MY_KEY = process.env.REACT_APP_API_KEY;
 app.get("/", (req, res) =>
   axios({
     method: "get",
-    url: "https://api.tfgm.com/odata/Metrolinks",
+    url: `https://api.tfgm.com/odata/Metrolinks?$filter=StationLocation%20eq%20\'Timperley\'`,
     headers: {
       "Ocp-Apim-Subscription-Key": MY_KEY,
     },
   })
-    .then((res) => console.log(res.data.value[0]["StationLocation"]))
+    .then((res) => console.log(res.data.value))
     .catch((err) => console.log(err))
 );
-console.log(MY_KEY);
 module.exports = app;
