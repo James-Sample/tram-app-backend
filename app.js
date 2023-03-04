88b2f5c21e2f1c98a8d870078f984a4c63c37483
James Goodman <jamesgoodman206@gmail.com>
2023-03-04 12:44:53 +0000

    got backend data

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 const MY_KEY = process.env.REACT_APP_API_KEY;
 
-app.get("/", (req, res) =>
+app.get("/", (_, res) =>
   axios({
     method: "get",
     url: `https://api.tfgm.com/odata/Metrolinks?$filter=StationLocation%20eq%20\'Timperley\'`,
@@ -13,7 +16,7 @@ app.get("/", (req, res) =>
       "Ocp-Apim-Subscription-Key": MY_KEY,
     },
   })
-    .then((res) => console.log(res.data.value))
-    .catch((err) => console.log(err))
+    .then((payload) => res.status(200).json(payload.data.value))
+    .catch((err) => res.status(500).json(err))
 );
 module.exports = app;
