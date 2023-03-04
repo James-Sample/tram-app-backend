690d48b167f58e20659f3263ecabd198dcc31373
James Sample <jamesgsample@gmail.com>
2023-03-04 16:41:07 +0000

    filtering individual stations

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ app.use(cors());
 
 const MY_KEY = process.env.REACT_APP_API_KEY;
 
-app.get("/", (_, res) =>
+app.get("/", (req, res) =>
   axios({
     method: "get",
-    url: `https://api.tfgm.com/odata/Metrolinks?$filter=StationLocation%20eq%20\'Timperley\'`,
+    url: `https://api.tfgm.com/odata/Metrolinks?$filter=StationLocation%20eq%20\'${req.query.station}\'`,
     headers: {
       "Ocp-Apim-Subscription-Key": MY_KEY,
     },
